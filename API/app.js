b40eb2d38501fdc7ac3ce5bc678d113b63101bd2
MSanouvo <43829596+MSanouvo@users.noreply.github.com>
2025-04-28 03:05:35 -0700

    feat: set up prisma with planned database schema
    - projected schema subject to change during dev

---
+++ API/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server Running at port: ${PORT}`)
+})
