324d6e1eb987c1ee5884d7a6e24c8aea064b1087
MSanouvo <43829596+MSanouvo@users.noreply.github.com>
2025-05-04 16:08:54 -0700

    feat: testing cors fix

--- API/app.js
+++ API/app.js
@@ -8,6 +8,13 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: "secret", resave: false, saveUninitialized: false }))
 app.use(controller.passport.session())
