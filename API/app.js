ddcd70dcf8c9af7374f9eb42938712bc6a427645
MSanouvo <43829596+MSanouvo@users.noreply.github.com>
2025-05-04 11:02:23 -0700

    feat: added run script command for deployment

--- API/app.js
+++ API/app.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const path = require('node:path')
 const router = require('./routes/routes')
 const session = require("express-session")
 const controller = require('./controller/controller')
 
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: "secret", resave: false, saveUninitialized: false }))
@@ -17,5 +16,5 @@ app.use(express.urlencoded({ extended: false}))
 app.use('/', router)
 
 app.listen(PORT, () =>{
-    console.log(`Server Running at port: ${PORT}`)
+    console.log(`Server Running on port: ${PORT}`)
 })
